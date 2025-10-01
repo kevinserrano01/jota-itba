2bca879fa2a71a091fd4d41c2804e81335a1e8d9
geroadrbosco <geronimoadribosco@gmail.com>
2025-10-01 19:21:26 -0300

    Añadi un middleware de error 404

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,10 @@ app.use((req, res, next) => {
 app.use("/api/productos", productosRouter);
 
 app.use((err, req, res, next) => {
+  res.status(404).json({ error: "Ruta no encontrada" });
+  });
+
+app.use((err, req, res, next) => {
   console.error(`[ERROR] ${err.message}`);
   res.status(err.status || 500).json({ error: err.message || "Error interno del servidor" });
   });
