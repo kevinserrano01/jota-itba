349e20e96eba7f79a9f37989559607e2318ca8aa
geroadrbosco <geronimoadribosco@gmail.com>
2025-10-01 18:47:30 -0300

    Añadi un Middleware de logging global

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const { productosRouter } = require("./index");
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`[LOG] ${req.method} ${req.url}`);
+  next();
+});
+
 app.use("/api/productos", productosRouter);
 
 app.listen(3001, () => {
