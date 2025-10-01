9698c74b8c071dbfe36f7dd54e2a5d4a28d0190d
geroadrbosco <geronimoadribosco@gmail.com>
2025-10-01 19:15:26 -0300

    Instale CORS y lo implemente como middleware

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+app.use(cors({ origin: "http://localhost:3000" }));
+
 app.use((req, res, next) => {
   console.log(`[LOG] ${req.method} ${req.url}`);
   next();
