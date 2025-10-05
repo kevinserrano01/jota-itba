87af19635f60a7675f699bee9c7f50d0accbac6b
Romero Fernando <agustinfernando2000@hotmail.com>
2025-10-04 23:00:50 -0300

    App funcionando en single page

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -2,48 +2,23 @@ import React from "react";
 import '../css/Navbar.css'
 import { useState } from "react";
 
-function Navbar() {
-    const [paginaActual, setPagina] = useState("Inicio")
-
-    const irAPagina = (pagina) => {
-        setPagina(pagina);
-    };
-
-    const visualizarPagina = () => {
-        switch (paginaActual) {
-            case "inicio":
-                return <h1> inicio </h1>
-            // return <Inicio></Inicio>;
-            case "productos":
-                return <h1> productos </h1>
-            // return <Productos></Productos>;
-            case "carrito":
-                return <h1> carrito </h1>
-            // return <Carrito></Carrito>;
-            case "contacto":
-                return <h1> contacto </h1>
-            // return <Contacto></Contacto>;
-
-        }
-    }
-    return (
-        <>
-        <nav className="navbar">
-            <img src="https://raw.githubusercontent.com/fromero34/img-frontend/093bd10a251a63e0cf51e0f38237d102f37d5c04/logo.svg" alt="Logo" id="logo" onClick={() => irAPagina('inicio')}style={{ cursor: 'pointer' }} ></img>
-            <div className="navbar-botones">
-            <button onClick={() => irAPagina('inicio')}>Inicio</button>
-            <button onClick={() => irAPagina('productos')}>Producto</button>
-            <button onClick={() => irAPagina('carrito')}>Carrito</button>
-            <button onClick={() => irAPagina('contacto')}>Contacto</button>
-            </div>
-        </nav>
-        <main className="main">
-            {visualizarPagina()}
-        </main>
-        </>
-    )
+function Navbar({ setPagina }) {
+  return (
+    <nav className="navbar">
+      <img
+        src="https://raw.githubusercontent.com/fromero34/img-frontend/093bd10a251a63e0cf51e0f38237d102f37d5c04/logo.svg"
+        alt="Logo"
+        id="logo"
+        onClick={() => setPagina("inicio")}
+        style={{ cursor: "pointer" }}
+      />
+      <div className="navbar-botones">
+        <button onClick={() => setPagina("inicio")}>Inicio</button>
+        <button onClick={() => setPagina("productos")}>Productos</button>
+        <button onClick={() => setPagina("carrito")}>Carrito</button>
+        <button onClick={() => setPagina("contacto")}>Contacto</button>
+      </div>
+    </nav>
+  );
 }
-
-/* 
-*/
 export default Navbar;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,29 @@ import Navbar from '../components/Navbar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [paginaActual, setPagina] = useState("inicio");
+
+  const visualizarPagina = () => {
+    switch (paginaActual) {
+      case "inicio":
+        return <h1> Inicio </h1>
+      case "productos":
+        return <h1> productos </h1>
+      case "carrito":
+        return <h1> carrito </h1>
+      case "contacto":
+        return <h1> contacto </h1>
+      default:
+        return <h1> Inicio </h1>
+    }
+  };
   return (
     <>
-    <div>
-      <Navbar></Navbar>
-    </div>
+      <Navbar setPagina={setPagina} />
+      <main className="main">{visualizarPagina()}</main>
     </>
-  )
+  );
 }
+
 
 export default App
