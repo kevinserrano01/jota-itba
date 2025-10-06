8b0d52a92f3ceaffcb249605c970afc2fa85e98a
geroadrbosco <geronimoadribosco@gmail.com>
2025-10-05 22:03:12 -0300

    Fix de Middlewares

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const { productosRouter } = require("./index");
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: ["http://localhost:3000", "http://localhost:5501"] }));
 
 app.use((req, res, next) => {
   console.log(`[LOG] ${req.method} ${req.url}`);
@@ -14,9 +14,9 @@ app.use((req, res, next) => {
 
 app.use("/api/productos", productosRouter);
 
-app.use((err, req, res, next) => {
+app.use((req, res, next) => {
   res.status(404).json({ error: "Ruta no encontrada" });
-  });
+});
 
 app.use((err, req, res, next) => {
   console.error(`[ERROR] ${err.message}`);
