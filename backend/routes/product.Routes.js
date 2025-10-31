76889ef7395c677702ee6db31ac5e1449be816b9
geroadrbosco <geronimoadribosco@gmail.com>
2025-10-31 18:26:48 -0300

    Migre el CRUD de productos de index.js a un router

---
+++ backend/routes/product.Routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const productoController = require("../controllers/producto.controller.js");
+
+router.post("/", productoController.createProducto);
+router.get("/", productoController.getProductos);
+router.get("/:id", productoController.getProductoById);
+router.put("/:id", productoController.updateProducto);
+router.delete("/:id", productoController.deleteProducto);
+
+module.exports = router;
